d21855fc8b07d33e494d1439e8e9be1977c6191b
Deepak <hdeepak5902@gmail.com>
2024-05-20 00:35:56 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useState } from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images'
+import './Navbar.css';
+
+const Navbar = () => {
+
+	const [toggle, setToggle] = useState(false)
+
+
+	return (
+		<div className='app__navbar'>
+		  <div className='app__navbar-logo'>
+			<img src={images.gericht} alt="logo" />
+		  </div>
+		  <ul className='app__navbar-links'>
+			  <li className='p__opensans'><a href="#home">Home</a></li>
+			  <li className='p__opensans'><a href="#about">About</a></li>
+			  <li className='p__opensans'><a href="#menu">Menu</a></li>
+			  <li className='p__opensans'><a href="#awards">Awards</a></li>
+			  <li className='p__opensans'><a href="#contact">Contact</a></li>
+		  </ul>
+		  <div className='app__navbar-login'>
+			<a href="#login" className='p__opensans'>Log In / Register</a>
+			<div></div>
+			<a href="/" className='p__opensans'>Book Table</a>
+		  </div>
+		  	
+		  <div className='app__navbar-smallscreen'>
+		  		<GiHamburgerMenu fontSize={27} color="#fff" onClick={()=>setToggle(true)}/>
+				{toggle && (
+
+				<div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+				<MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={()=>setToggle(false)}/>
+					<ul className='app__navbar-smallscreen_links'>
+						<li><a onClick={()=>setToggle(false)} href="#home">Home</a></li>
+						<li><a onClick={()=>setToggle(false)} href="#about">About</a></li>
+						<li><a onClick={()=>setToggle(false)} href="#menu">Menu</a></li>
+						<li><a onClick={()=>setToggle(false)} href="#awards">Awards</a></li>
+						<li><a onClick={()=>setToggle(false)} href="#contact">Contact</a></li>
+					</ul>
+				</div>
+				)}
+		  </div>
+		</div>
+	  );
+}
+
+
+
+export default Navbar;
+
+
+{/* <GiHamburgerMenu color='#fff' fontSize={27} onClick={()=>{}}/>
+		<div className='app__navbar-smallscreen_overlay flex_center slide-bottom'>
+		<MdOutlineRestaurantMenu fontSize={27} className="overlay_-close" onClick={()=>{}}/>
+		<ul className='app__navbar-smallscreen-links'>
+			<li className='p__opensans'><a href="#home">Home</a></li>
+			<li className='p__opensans'><a href="#about">About</a></li>
+			<li className='p__opensans'><a href="#menu">Menu</a></li>
+			<li className='p__opensans'><a href="#awards">Awards</a></li>
+			<li className='p__opensans'><a href="#contact">Contact</a></li>
+		</ul>
+		</div> */}
